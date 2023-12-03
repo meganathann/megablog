6273b77bdaeb1589f1e4dcbbadd18edcedc873f0
Meganathan N <meganathanmaggi25@gmail.com>
2023-12-03 19:04:31 +0530

    9th commit

--- Backend/Helpers/database/connectDatabase.js
+++ Backend/Helpers/database/connectDatabase.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = async () => {
-  await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
-  console.log("MongoDB Connection Successfully");
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connection Successfully");
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error);
+  }
 };
 
 module.exports = connectDatabase;
