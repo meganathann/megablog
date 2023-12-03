4fa14030b22d38cd2dfcedac62f282380951608d
Meganathan N <meganathanmaggi25@gmail.com>
2023-12-03 17:09:53 +0530

    3

--- Backend/Helpers/database/connectDatabase.js
+++ Backend/Helpers/database/connectDatabase.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = async () => {
-  await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+  await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   console.log("MongoDB Connection Successfully");
 };
 
